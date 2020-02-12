eba4bc83bfed24d8ba49be885ac61c22149617db
Ben McMann <bbmcmann@volosys.com>
2020-02-11 23:30:55 -0700

    Fin a4 i think

--- src/IntArrayWorker.java
+++ src/IntArrayWorker.java
@@ -85,4 +85,36 @@ public class IntArrayWorker {
 		}
 	}
 
+	public int getCount(int x){
+		int total = 0;
+		for(int[] rowArray : matrix){
+			for(int spot : rowArray){
+				if(spot == x){
+					total++;
+				}
+			}
+		}
+		return total;
+	}
+
+	public int getLargest(){
+		int largest = matrix[0][0];
+		for(int[] rowArray : matrix){
+			for(int spot : rowArray){
+				if(spot > largest){
+					largest = spot;
+				}
+			}
+		}
+		return largest;
+	}
+
+	public int getColToal(int x){
+		int total =0;
+		for(int i = 0; i < matrix.length; i++){
+			total = total + matrix[i][x];
+		}
+		return total;
+	}
+
 }
